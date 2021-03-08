f8247d6e506515e7b2c0faf319cc331614f91c4d
Motherlode Finance <motherlodefinance@protonmail.com>
2021-03-08 22:01:00 +1100

    Initial Import

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,96 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://www.motherlode.finance/'
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap'
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool'
+      }
+    ]
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: 'https://www.motherlode.finance/farms'
+  },
+  {
+    label: 'Nests',
+    icon: 'PoolIcon',
+    href: 'https://www.motherlode.finance/nests'
+  },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: 'https://www.motherlode.finance/lottery'
+  // },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'PancakeSwap',
+        href: 'https://pancakeswap.info/token/0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+      },
+      {
+        label: 'CoinGecko',
+        href: 'https://www.coingecko.com/en/coins/motherlode-finance',
+      },
+      {
+        label: 'CoinMarketCap',
+        href: 'https://coinmarketcap.com/currencies/motherlode-finance/',
+      },
+      {
+        label: 'AstroTools',
+        href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
+      },
+    ]
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: "Github",
+        href: "https://github.com/motherlodedefi/",
+      },
+      {
+        label: "Docs",
+        href: "https://motherlodedefi.gitbook.io/motherlode-finance/",
+      },
+      {
+        label: "Blog",
+        href: "https://motherlodefinance.medium.com/",
+      },
+    ],
+  },
+  {
+    label: 'Partnerships/IFO',
+    icon: 'MotherlodeIcon',
+    href: 'https://docs.google.com/forms/d/e/1FAIpQLSe7ycrw8Dq4C5Vjc9WNlRtTxEhFDB1Ny6jlAByZ2Y6qBo7SKg/viewform?usp=sf_link',
+  },
+  {
+    label: 'Audit by Hacken',
+    icon: 'AuditIcon',
+    href: 'https://www.motherlode.finance/files/hackenAudit.pdf',
+  },
+  {
+    label: 'Audit by CertiK',
+    icon: 'AuditIcon',
+    href: 'https://certik.org/projects/motherlode-finance',
+  },
+]
+
+export default config
